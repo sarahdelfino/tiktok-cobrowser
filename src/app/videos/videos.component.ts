d3a6e8debe6b4d714c728bb477da87ad9a36580f
Sarah <sarahdelfino7@gmail.com>
2020-05-06 15:06:33 -0400

    WIP

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,25 +1,16 @@
-import { Component, Injectable, Inject, ElementRef } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.css']
 })
-@Injectable()
-export class VideosComponent{
 
-  constructor(@Inject(DOCUMENT) private document: Document, private elementRef: ElementRef) { }
+export class VideosComponent implements OnInit {
 
-  // ngAfterViewInit() {
-  //   var s = this.document.createElement('blockquote');
-  //   s.className="tiktok-embed"
-  //   s.cite="https://www.tiktok.com/@mojothemonkey/video/6822698712737844485"
-  //   s.style.width = '100%';
-  //   s.style.height = 'auto';
-  //   this.elementRef.nativeElement.appendChild(s);
-  // }
+  constructor() { }
 
-  
-
+  ngOnInit() {
+    
+  }
 }
