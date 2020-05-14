03549d80fcfe779b75a27210a0d161c2bd893fd5
Sarah <sarahdelfino7@gmail.com>
2020-05-14 14:31:37 -0400

    UI changes

--- src/app/mock-playlist.ts
+++ src/app/mock-playlist.ts
@@ -3,5 +3,7 @@ import { Playlist } from './playlist';
 export const PLAYLISTS: Playlist[] = [
     { name: 'playlist 1', url: 'https://www.tiktok.com/oembed?url=https://www.tiktok.com/@scout2015/video/6718335390845095173'},
     { name: 'playlist 2', url: 'https://www.tiktok.com/oembed?url=https://www.tiktok.com/@oakleythegoldenpuppy/video/6816357733827218693'},
-    { name: 'blank playlist' }
+    { name: 'hello' },
+    { name: 'yep' },
+    { name: 'alright' },
 ];
